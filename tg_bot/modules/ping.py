416f0b6d70e53497a34cf89a68730aa21fd9d8c8
ridhoartha <83918943+ridhoartha@users.noreply.github.com>
2021-07-07 15:32:09 +0700

    Update ping.py

--- tg_bot/modules/ping.py
+++ tg_bot/modules/ping.py
@@ -72,10 +72,8 @@ def ping(bot: Bot, update: Update):
     telegram_ping = ping_func(["Telegram"])[0].split(": ", 1)[1]
     uptime = get_readable_time((time.time() - StartTime))
 
-    reply_msg = ("PONG!!\n"
-                 "<b>Time Taken:</b> <code>{}</code>\n"
-                 "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime))
-
+    reply_msg = ("Pong!\n"
+                 "</b> <code>{}</code>\n")
     update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
 
 
